e76f92c3a8e3fb723f4fe51c5f153977712e2b19
Chris Tonog <chris.tonog@gmail.com>
2012-11-03 13:56:42 -0400

    first prompt of game

---
+++ README.md
@@ -0,0 +1,2 @@
+This is my game. The current working title is "game." I don't care if it sucks.
+
--- game.py
+++ game.py
@@ -1,1 +1,29 @@
-print "This is my game."
+from sys import exit
+
+prompt = "> "
+
+def dark_forest():
+	print "You have entered the dark forest."
+
+print "Welcome to the dark forest."
+print "Do you want to enter the dark forest?"
+
+response = raw_input(prompt)
+
+if response.lower() == "yes":
+	print "Great! let's get started..."
+	dark_forest()
+elif response.lower() == "no":
+	print "Good bye."
+else:
+	while True:
+		if response.lower() == "yes":
+			dark_forest()
+		elif response.lower() == "no":
+			print "good bye".capitalize()
+			exit(1)
+		else:
+			print "Enter yes or no"
+			response = raw_input(prompt)
+
+
